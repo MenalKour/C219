c9b54e5325612c102a768c87c1d08d94b395f098
MenalKour <menalkaur2003@gmail.com>
2022-09-30 16:53:58 +0530

    added

--- server.js
+++ server.js
@@ -4,20 +4,36 @@ const server = require("http").Server(app);
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+const { v4: uuidv4 } = require("uuid");
+
 const io = require("socket.io")(server, {
     cors: {
         origin: '*'
     }
 });
 
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+    debug: true,
+});
+
+app.use("/peerjs", peerServer);
+
 app.get("/", (req, res) => {
-    res.render("index");
+    res.redirect(`/${uuidv4()}`);
+});
+
+app.get("/:room", (req, res) => {
+    res.render("index", { roomId: req.params.room });
 });
 
 io.on("connection", (socket) => {
-    socket.on("message", (message, user) => {
-        io.emit("createMessage", message, user);
+    socket.on("join-room", (roomId, userId, userName) => {
+        socket.join(roomId);
+        socket.on("message", (message) => {
+            io.to(roomId).emit("createMessage", message, userName);
+        });
     });
 });
 
-server.listen(3030);
+server.listen(process.env.PORT || 3030);
